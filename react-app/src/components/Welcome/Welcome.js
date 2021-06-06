345a9e98f50453cb2715a19ac2cffe96bd9c99a0
Laura <lkzailac@gmail.com>
2021-06-06 16:40:05 -0400

    consumer side styled

--- react-app/src/components/Welcome/Welcome.js
+++ react-app/src/components/Welcome/Welcome.js
@@ -11,71 +11,74 @@ import "./welcome.css"
 const Welcome = () => {
   return (
     <>
-        <div className='welcome-container'>
-            <div className='background-image-container'>
-                <img id='background-image' src={background_img} />
+        <div className='outer-div'>
+            <div className='welcome-container'>
+                <div className='background-image-container'>
+                    <img id='background-image' src={background_img} />
+                </div>
+                <div className='content-grid'>
+
+
+                    <div className='left-container'>
+                        <div className='left-for'>
+                            <h2>For</h2>
+                        </div>
+                        <div className='left-group'>
+                            <h1>Companies</h1>
+                        </div>
+                        <div className='left-p'>
+                            <p>A platform where companies can track and share their carbon outputs<br></br>and set goals for the future of commerce.</p>
+                        </div>
+                        <div className='arrow-container'>
+                            <img className='left-arrow bounce' src={leftArrow} />
+                        </div>
+                        <div className="join-container">
+                            <div className='join'>
+                                <Link className='join-link' to='/company-signup'>
+                                    JOIN NOW
+                                </Link>
+                            </div>
+                        </div>
+                        <div className='login-container'>
+                            <div className='login'>
+                                <Link className='login-link' to='/company-login'>
+                                    SIGN IN
+                                </Link>
+                            </div>
+                        </div>
+                    </div> {/* end left-container */}
+
+                    <div className='right-container'>
+                        <div className='right-for'>
+                            <h2>For</h2>
+                        </div>
+                        <div className='right-group'>
+                            <h1>Consumers</h1>
+                        </div>
+                        <div className='right-p'>
+                            <p>A place where consumers can find companies whose plans <br></br>for the future align with their own. </p>
+                        </div>
+                        <div className='right-arrow-container'>
+                            <img className='right-arrow right-bounce' src={largeArrow} />
+                        </div>
+                        <div className="r-join-container">
+                            <div className='r-join'>
+                                <Link className='r-join-link' to='/sign-up'>
+                                    JOIN NOW
+                                </Link>
+                            </div>
+                        </div>
+                        <div className='r-login-container'>
+                            <div className='r-login'>
+                                <Link className='r-login-link' to='/login'>
+                                    SIGN IN
+                                </Link>
+                            </div>
+                        </div>
+                    </div> {/* end right-container */}
+                </div>  {/* end content grid */}
             </div>
-
-            <div className='left-container'>
-                <div className='left-for'>
-                    <h2>For</h2>
-                </div>
-                <div className='left-group'>
-                    <h1>Companies</h1>
-                </div>
-                <div className='left-p'>
-                    <p>A platform where companies can track and share their carbon outputs<br></br>and set goals for the future of commerce.</p>
-                </div>
-                <div className='arrow-container'>
-                    <img className='left-arrow bounce' src={leftArrow} />
-                </div>
-                <div className="join-container">
-                    <div className='join'>
-                        <Link className='join-link' to='/company-signup'>
-                            JOIN NOW
-                        </Link>
-                    </div>
-                </div>
-                <div className='login-container'>
-                    <div className='login'>
-                        <Link className='login-link' to='/company-login'>
-                            SIGN IN
-                        </Link>
-                    </div>
-                </div>
-            </div> {/* end left-container */}
-
-            <div className='right-container'>
-                <div className='right-for'>
-                    <h2>For</h2>
-                </div>
-                <div className='right-group'>
-                    <h1>Consumers</h1>
-                </div>
-                <div className='right-p'>
-                    <p>A place where consumers can find companies whose plans <br></br>for the future align with their own. </p>
-                </div>
-                <div className='right-arrow-container'>
-                    <img className='right-arrow bounce' src={largeArrow} />
-                </div>
-                <div className="r-join-container">
-                    <div className='r-join'>
-                        <Link className='r-join-link' to='/sign-up'>
-                            JOIN NOW
-                        </Link>
-                    </div>
-                </div>
-                <div className='r-login-container'>
-                    <div className='r-login'>
-                        <Link className='r-login-link' to='/login'>
-                            SIGN IN
-                        </Link>
-                    </div>
-                </div>
-            </div> {/* end right-container */}
-
         </div>
-
     </>
   );
 }
