0975b6b1acd69c27820e3498dbee983f57737600
Laura <lkzailac@gmail.com>
2021-06-13 17:46:39 -0400

    search finished

--- react-app/src/components/ConsumerPage/ConsumerPage.js
+++ react-app/src/components/ConsumerPage/ConsumerPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
 import { getAll } from '../../store/allproducts';
+import Search from '../Search/index';
 
 import downArrow from "../../images/down-arrow.svg";
 import './consumerpg.css';
@@ -12,60 +12,53 @@ const ConsumerPage = () => {
     const user = useSelector(state => state.session.user)
     const allgroup = useSelector(state => state.allproducts.all)
     const all = allgroup?.all
-    const [searchInput, setSearchInput] = useState('')
+    const [term, setTerm] = useState('')
+
 
     useEffect(() => {
        dispatch(getAll(user.id))
     }, [dispatch])
 
-    const handleSearch =() => {
-
+    ////////////LOGIC FOR SEARCH
+    const callback = (term ) => {
+        setTerm(term)
     }
 
-    /////////// RENDER COMPANY + ALL PRODUCTS
-    const companyAndProducts = () => (
-        <div className='all-products-inner-contain'>
-        {
-            all?.map((groups, i) => {
-                return (
-                    <div key={i} className='company-product-group'>
-                        <div className='company-name-contain'>
-                            <h1 className='company-name'>{groups[0]?.name}</h1>
-                        </div>
-                        <div className='companies-products'>
-                        {
-                            groups[1]?.map((item, j) => {
-                                return (
-                                    <div className='item-container'>
-                                        <div className='single-company-products'>
-                                            <h2 key={j} className='prod-name'>{item.name}</h2>
-                                            <div className='product-photo_cp'>
-                                                <img src={item.photo_url} alt="product image"/>
-                                            </div>
-                                        </div>
-                                        <div className='prod-info'>
-                                            <div className='footprint'>
-                                                Carbon Footprint:
-                                                <p>{item.carbon_footprint_kg} kg CO<span>&#8322;</span>e</p>
-                                            </div>
-                                            <div className='returnable'>
-                                                Returnable at the end of life?
-                                                {item.returnable === true ?
-                                                <p>Yes, this item can be returned <br></br>to {groups[0]?.name}.</p> :
-                                                <p>No, please consider repairing or recycling.</p>}
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
-                        </div>
+    const filterIt = (item, searchTerm) => {
+        return item?.name.toLowerCase().includes(searchTerm.toLowerCase())
+                || item?.company_name.toLowerCase().includes(searchTerm.toLowerCase())
+                || item?.product_category.toLowerCase().includes(searchTerm.toLowerCase())
+    }
+
+
+    const searchResults = all?.filter((item) => filterIt(item, term))?.map((item, j) => {
+        return (
+            <div className='item-container'>
+                <div className='single-company-products'>
+                    <h3>{item.company_name}</h3>
+                    <div className='product-photo_cp'>
+                        <img src={item.photo_url} alt="product image"/>
                     </div>
-                )
-            })
-        }
-        </div>
-    )
+                    <div className='p-name-contain'>
+                        <h2 key={j} className='prod-name'>{item.name}</h2>
+                    </div>
+                </div>
+                <div className='prod-info'>
+                    <div className='footprint'>
+                        Carbon Footprint:
+                        <p>{item.carbon_footprint_kg} kg CO<span>&#8322;</span>e</p>
+                    </div>
+                    <div className='returnable'>
+                        Returnable at the end of life?
+                        {item.returnable === true ?
+                        <p>Yes, this item can be returned <br></br>to {item.company_name}.</p> :
+                        <p>No, please consider repairing or recycling.</p>}
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
 
 
     return (
@@ -80,7 +73,8 @@ const ConsumerPage = () => {
                     </div>
 
                     <div className='search-container'>
-                        <form className='search-form' onSubmit={handleSearch}>
+                        <Search callback={callback}/>
+                        {/* <form className='search-form' onSubmit={handleSearch}>
                             <input
                             type="text"
                             placeholder='Search for a Company or Product'
@@ -88,12 +82,12 @@ const ConsumerPage = () => {
                             onChange={(e) => setSearchInput(e.target.value)}
                             >
                             </input>
-                        </form>
+                        </form> */}
                     </div>
                 </div>
 
                 <div className='all-prod-container'>
-                {companyAndProducts()}
+                {searchResults}
                 </div>
 
             </div> {/* end consumer-page-content*/}
